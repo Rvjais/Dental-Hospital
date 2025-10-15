08d18fb63fef691c6768d717a7e20ed1d6bbc544
Rvjais <ranveerjais9984@gmail.com>
2025-10-15 19:26:41 +0530

    added hashroutes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, HashRouter} from 'react-router-dom'
 import HomePage from './components/pages/Home/index.jsx'
 import AboutPage from './components/pages/About/index.jsx'
 
 const AppRoutes = () => {
   return (
     <>
+    <HashRouter>
+   
 <BrowserRouter>
-
 <Routes>
  <Route path='/' element={<HomePage/>} />
  <Route path='/about' element={<AboutPage/>} />
 </Routes>
 </BrowserRouter>
+   
+    </HashRouter>
     </>
   )
 }
